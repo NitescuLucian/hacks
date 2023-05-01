e46a4bce285820360c7b4c0c4280e09afe6732ef
NitescuLucian <nitesculuci@gmail.com>
2023-05-01 21:21:58 +0300

    new tool, idq

---
+++ idq/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		input := scanner.Text()
+		if input == "" {
+			continue
+		}
+
+		// Get the IP address of the domain name
+		ip, err := net.LookupIP(input)
+		if err != nil {
+			continue
+		}
+		// Query Shodan for the IP address
+		resp, err := http.Get(fmt.Sprintf("https://internetdb.shodan.io/%s", ip[0].String()))
+		if err != nil {
+			continue
+		}
+		defer resp.Body.Close()
+		// Parse the JSON response
+		var result map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+			continue
+		}
+		// Print the ports if they exist
+		if ports, ok := result["ports"]; ok {
+			for _, port := range ports.([]interface{}) {
+				fmt.Printf("%s:%d\n", input, int(port.(float64)))
+			}
+		}
+	}
+
+}
---
+++ idq/readme.md
@@ -0,0 +1,15 @@
+# idq
+
+## Why? 
+
+To port scan using internetdb.shodan.io.
+
+## Install
+
+You can install using go:
+
+```
+go install -v github.com/NitescuLucian/hacks/idq@latest
+```
+
+Contact me at [@LucianNitescu](https://twitter.com/LucianNitescu)
